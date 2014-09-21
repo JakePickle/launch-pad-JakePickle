c74c3b650df12f04092c642db3e0b774ba270aff
JakePickle <jmp6xb@mst.edu>
2014-09-21 15:55:00 -0500

    Added this.DataContext = new MainWindowViewModel(); line to MainWindow.xaml.cs

--- LaunchPadJakePickle/LaunchPadJakePickle/MainWindow.xaml.cs
+++ LaunchPadJakePickle/LaunchPadJakePickle/MainWindow.xaml.cs
@@ -18,11 +18,15 @@ namespace LaunchPadJakePickle
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
+    using ViewModels;
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new MainWindowViewModel();
         }
     }
 }
