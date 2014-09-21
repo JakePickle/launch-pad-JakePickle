71cf4581925403e1368c326aa8cd57302bacd699
JakePickle <jmp6xb@mst.edu>
2014-09-21 15:56:32 -0500

    Added DataContext = ViewModel for ConsoleView

--- LaunchPadJakePickle/LaunchPadJakePickle/Views/ConsoleView.xaml.cs
+++ LaunchPadJakePickle/LaunchPadJakePickle/Views/ConsoleView.xaml.cs
@@ -18,11 +18,15 @@ namespace LaunchPadJakePickle.Views
     /// <summary>
     /// Interaction logic for ConsoleView.xaml
     /// </summary>
+
+    using ViewModels;
+
     public partial class ConsoleView : UserControl
     {
         public ConsoleView()
         {
             InitializeComponent();
+            this.DataContext = new ConsoleViewModel();
         }
     }
 }
