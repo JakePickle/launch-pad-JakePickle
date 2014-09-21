ad9acdf8162ba06ce1647e479150fbb62a96f8ff
JakePickle <jmp6xb@mst.edu>
2014-09-21 16:05:39 -0500

    Added private MainWindowModel model to MainWindowViewModel.cs

--- LaunchPadJakePickle/LaunchPadJakePickle/ViewModels/MainWindowViewModel.cs
+++ LaunchPadJakePickle/LaunchPadJakePickle/ViewModels/MainWindowViewModel.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace LaunchPadJakePickle.ViewModels
 {
+
+    using Models;
+
     class MainWindowViewModel
     {
+        private MainWindowModel Model = new MainWindowModel();
     }
 }
