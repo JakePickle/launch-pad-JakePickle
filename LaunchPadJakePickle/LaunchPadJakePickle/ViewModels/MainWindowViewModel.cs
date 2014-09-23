db0c35ef8170ae5e940ea7d4c9fbc81f958e1f0f
JakePickle <jmp6xb@mst.edu>
2014-09-22 19:13:47 -0500

    Usings in MainWindowViewModel.cs removed

--- LaunchPadJakePickle/LaunchPadJakePickle/ViewModels/MainWindowViewModel.cs
+++ LaunchPadJakePickle/LaunchPadJakePickle/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using LaunchPadJakePickle.Models;
-using Caliburn.Micro;
+﻿using LaunchPadJakePickle.Models;
 
 namespace LaunchPadJakePickle.ViewModels
 {
