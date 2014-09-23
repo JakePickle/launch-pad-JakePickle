8adf0a2cc23109e44baa7b2a5f4d267565f4e6b9
JakePickle <jmp6xb@mst.edu>
2014-09-22 19:11:16 -0500

    Fixed build errors in MainWindowViewModel.cs

--- LaunchPadJakePickle/LaunchPadJakePickle/ViewModels/MainWindowViewModel.cs
+++ LaunchPadJakePickle/LaunchPadJakePickle/ViewModels/MainWindowViewModel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using LaunchPadJakePickle.Models;
-using Caliburn.Micro.PropertyChangedBase;
+using Caliburn.Micro;
 
 namespace LaunchPadJakePickle.ViewModels
 {
