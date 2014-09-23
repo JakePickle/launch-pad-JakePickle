31108d4ca9ed0295065a8b96e953d618119e94f7
JakePickle <jmp6xb@mst.edu>
2014-09-23 09:04:25 -0500

    Created Text property in ConsoleViewModel.cs

--- LaunchPadJakePickle/LaunchPadJakePickle/ViewModels/ConsoleViewModel.cs
+++ LaunchPadJakePickle/LaunchPadJakePickle/ViewModels/ConsoleViewModel.cs
@@ -1,10 +1,25 @@
 ﻿using LaunchPadJakePickle.Models;
+using Caliburn.Micro;
 
 namespace LaunchPadJakePickle.ViewModels
 {
-    class ConsoleViewModel
+    class ConsoleViewModel : PropertyChangedBase
     {
         private ConsoleModel Model;
+
+        public string Text
+        {
+            get
+            {
+                return Model.text;
+            }
+            set
+            {
+                Model.text = value;
+                NotifyOfPropertyChange(() => Model.text);
+            }
+        }
+
         public ConsoleViewModel()
         {
             Model = new ConsoleModel();
