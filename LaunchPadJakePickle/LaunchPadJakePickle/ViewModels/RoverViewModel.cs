b649a24ca3a2644e60d1c4e1bcd5fb4df41ce0c9
JakePickle <jmp6xb@mst.edu>
2014-09-27 20:31:41 -0500

    Added properties in RoverViewModel for internal fields in RoverModel (roverName,speed,temperature,isConnected)

--- LaunchPadJakePickle/LaunchPadJakePickle/ViewModels/RoverViewModel.cs
+++ LaunchPadJakePickle/LaunchPadJakePickle/ViewModels/RoverViewModel.cs
@@ -5,7 +5,59 @@ namespace LaunchPadJakePickle.ViewModels
 {
     class RoverViewModel : PropertyChangedBase
     {
-        private RoverModel Model;
+        private RoverModel Model;      
+
+        public string roverName
+        {
+            get
+            {
+                return Model.roverName;
+            }
+            set
+            {
+                Model.roverName = value;
+                NotifyOfPropertyChange(() => Model.roverName);
+            }
+        }
+
+        public int speed
+        {
+            get
+            {
+                return Model.speed;
+            }
+            set
+            {
+                Model.speed = value;
+                NotifyOfPropertyChange(() => Model.speed);
+            }
+        }
+
+        public double temperature
+        {
+            get
+            {
+                return Model.temperature;
+            }
+            set
+            {
+                Model.temperature = value;
+                NotifyOfPropertyChange(() => Model.temperature);
+            }
+        }
+
+        public bool isConnected
+        {
+            get
+            {
+                return Model.isConnected;
+            }
+            set
+            {
+                Model.isConnected = value;
+                NotifyOfPropertyChange(() => Model.isConnected);
+            }
+        }
 
         public RoverViewModel()
         {
